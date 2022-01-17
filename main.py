ba0047c31b90e35ed7fd4607ef8175f9aebce68e
ShihabHT <shihabrahman122@gmail.com>
2022-01-17 13:11:15 +0600

    multiplication of 2 matrices, 2 different ways to define matrix

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+from numpy import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+arr = array([[1, 2, 3],[4, 5, 6]])
+m1 = matrix([[1, 2, 3],
+             [4, 5, 6]])
+m2 = matrix('1 2;'
+            ' 3 4;'
+            ' 5 6')
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(m1 * m2)
