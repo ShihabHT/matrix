0b42a3e0a36e5f375d7e8729a2aacebf6a3ef405
ShihabHT <shihabrahman122@gmail.com>
2022-02-05 23:28:47 +0600

    multiplication of 2 matrices, 2 different ways to define matrix

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from numpy import *
 
-arr = array([[1, 2, 3],[4, 5, 6]])
-m1 = matrix([[1, 2, 3],
-             [4, 5, 6]])
-m2 = matrix('1 2;'
-            ' 3 4;'
-            ' 5 6')
+# arr = array([[2, 0, -1],[4, 5, 6]])
+m1 = array([[2, 0, -1],
+             [5, 1, 0],
+             [0, 1, 3]])
+print(type(m1))
+m1 = matrix(m1)
+print(type(m1))
+m2 = matrix([[3, -1, 1],
+             [-15, 6, -5],
+             [5, -2, 2]])
+m3 = matrix('3 -1 1; -15 6 -5; 5 -2 2')
+print(m3)
+print(type(m2))
 print(m1 * m2)
+print(m2 * m1)
