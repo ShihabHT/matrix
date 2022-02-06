48c8975b505504cf67137b5a0e3de3967b95faeb
ShihabHT <shihabrahman122@gmail.com>
2022-02-06 21:24:16 +0600

    trying different operation on array and matrix

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 from numpy import *
 
-# arr = array([[2, 0, -1],[4, 5, 6]])
-m1 = array([[2, 0, -1],
-             [5, 1, 0],
-             [0, 1, 3]])
-print(type(m1))
-m1 = matrix(m1)
-print(type(m1))
+arr = array([[2, 0, -1, 1, 2, 3],
+             [4, 5, 6, 32, -5, 0]])
+print(arr)
+print("Data type : ", arr.dtype)
+print("Dimension : ", arr.ndim)
+print("Order of Array :", arr.shape)
+print("Length of entire block :", arr.size)
+
+print("\nThe method flatten() flattens any array and makes 1D array")
+arr2 = arr.flatten()
+print(arr2)
+
+print("\nThe method reshape() is reverse of flatten()")
+arr3 = arr2.reshape(3, 4)
+print(arr3)
+
+print("-----------------------------------------------------\n")
+print("\nAll operations that can be applied on array can also be applied on matrix")
+a = array([[2, 0, -1],
+           [5, 1, 0],
+           [0, 1, 3]])
+print(type(a))
+print("The function numpy.matrix() converts an array into a matrix")
+m = matrix(a)
+m1 = matrix('3 -1 1; -15 6 -5; 5 -2 2')
 m2 = matrix([[3, -1, 1],
              [-15, 6, -5],
              [5, -2, 2]])
-m3 = matrix('3 -1 1; -15 6 -5; 5 -2 2')
-print(m3)
-print(type(m2))
+print(m)
+print(type(m))
+print(m1)
+print("Diagonal of matrix m : ", diagonal(m))
+print(m1.min())
+print("Matrix operations")
+print(m1 + m2)
 print(m1 * m2)
-print(m2 * m1)
